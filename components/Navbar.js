74e637249bb47c951da1b80918aa046547bfc6b4
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-05-12 14:30:33 +0530

    sidenav

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,32 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import Sidenav from "./Sidenav";
 
 export default function Navbar() {
+  const [sidenav, setSidenav] = useState(false);
   return (
-    <div className="h-20 bg-slate-100 flex items-center px-6">
-      <button>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          class="w-6 h-6"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-          />
-        </svg>
-      </button>
-      <h1 className="text-xl text-slate-700 font-extrabold ml-5">
-        95<span className="font-light">Bills.com</span>
-      </h1>
-      <div className="ml-auto">
-        <button className=" bg-white px-4 py-2 rounded-md text-sm font-medium">
-          Create
+    <div className="h-20 bg-slate-50 flex items-center px-6">
+      <div className="flex items-center space-x-4 lg:hidden">
+        <button onClick={() => setSidenav(true)}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            />
+          </svg>
+        </button>
+        <h1 className="text-xl text-slate-700 font-extrabold">
+          95<span className="font-light">Bills.com</span>
+        </h1>
+      </div>
+      <div className="ml-auto font-poppins">
+        <button className="font-medium bg-white text-slate-700 px-4 py-2 rounded-md text-sm">
+          Generate
         </button>
       </div>
+      {sidenav && (
+        <div
+          onClick={() => setSidenav(false)}
+          className="fixed inset-0 h-screen w-screen bg-black/70 z-10"
+        >
+          <Sidenav />
+        </div>
+      )}
     </div>
   );
 }
