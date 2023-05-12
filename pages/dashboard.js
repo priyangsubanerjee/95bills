b55e230194d7ff33b3af04cecb4ba35f227a76e3
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-05-12 23:43:04 +0530

    tap color

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -75,7 +75,7 @@ function Dashboard() {
                 <path
                   stroke-linecap="round"
                   stroke-linejoin="round"
-                  d="M3.75 9h16.5m-16.5 6.75h16.5"
+                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
                 />
               </svg>
             </button>
