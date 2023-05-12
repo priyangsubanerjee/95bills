732f2f71a9cfa7471ed2b56d0c08ec0fe022536d
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-05-12 23:29:36 +0530

    grid

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -60,7 +60,7 @@ function Dashboard() {
 
       <div className="w-full h-full overflow-y-auto">
         {tab == 0 ? (
-          <div className="p-20">
+          <div className="lg:p-20 p-6">
             <h1 className="text-5xl text-slate-700 font-bold font-poppins">
               Invoices
             </h1>
@@ -68,60 +68,48 @@ function Dashboard() {
               Select an invoice to view details.
             </p>
             <div className="space-y-4 mt-10">
-              <div className="w-full h-20 bg-slate-50 rounded grid grid-cols-7 px-5 font-poppins">
-                <div className="flex items-center justify-center font-semibold text-slate-700 text-sm">
-                  <span className="font-light">#</span>RT3ED
-                </div>
-                <div className="flex items-center justify-center text-slate-600 text-sm">
-                  19th July 2022
-                </div>
-                <div className="flex justify-center items-center text-slate-600 text-sm">
-                  Aditya Kumar Jha
-                </div>
-                <div className="flex justify-center items-center text-slate-700 text-base font-semibold">
-                  ₹23459
-                </div>
-
-                <div className="flex justify-center items-center text-slate-600 text-sm">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width="1.5"
-                    stroke="currentColor"
-                    class="w-6 h-6"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zM3.75 12h.007v.008H3.75V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm-.375 5.25h.007v.008H3.75v-.008zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
-                    />
-                  </svg>
-                </div>
-                <div className="flex justify-center items-center text-slate-600 text-sm">
-                  <div className="flex font-medium items-center text-teal-500 justify-center space-x-3 p-3">
-                    <div className="h-3 w-3 rounded-full bg-teal-500"></div>
-                    <span>Paid</span>
+              <div className="w-full lg:hidden bg-slate-50 p-5 rounded-md">
+                <div className="flex items-center justify-between">
+                  <div>
+                    <span className="font-semibold text-slate-700 text-sm">
+                      <span className="font-light">#</span>RT3ED
+                    </span>
+                  </div>
+                  <div className="flex items-center justify-center text-slate-600 text-xs">
+                    19th July 2022
                   </div>
                 </div>
-                <div className="flex justify-center items-center text-slate-600 text-sm">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width="3"
-                    stroke="currentColor"
-                    class="w-5 h-5"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M8.25 4.5l7.5 7.5-7.5 7.5"
-                    />
-                  </svg>
+                <div className="mt-3 flex items-center justify-between">
+                  <div className="text-slate-600 text-sm">Aditya Kumar Jha</div>
+                  <div className="flex font-medium items-center text-yellow-500 justify-center space-x-3 p-3">
+                    <div className="h-3 w-3 rounded-full bg-yellow-500"></div>
+                    <span className="text-sm">Due</span>
+                  </div>
+                </div>
+                <div className="mt-3 flex items-center justify-between">
+                  <button className="flex items-center text-xs space-x-2 text-slate-500">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      class="w-4 h-4"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zM3.75 12h.007v.008H3.75V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm-.375 5.25h.007v.008H3.75v-.008zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
+                      />
+                    </svg>
+                    <span>View items</span>
+                  </button>
+                  <div className="flex justify-center items-center text-slate-700 text-base font-semibold">
+                    ₹23459
+                  </div>
                 </div>
               </div>
-              <div className="w-full h-20 bg-slate-50 rounded grid grid-cols-7 px-5 font-poppins">
+              <div className="w-full h-20 bg-slate-50 rounded hidden lg:grid grid-cols-7 px-5 font-poppins">
                 <div className="flex items-center justify-center font-semibold text-slate-700 text-sm">
                   <span className="font-light">#</span>RT3ED
                 </div>
