5a4bf1698c8a4a6c22966eb05d35cc6fd5fb13f6
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-05-12 18:31:40 +0530

    tab states

--- components/Sidenav.js
+++ components/Sidenav.js
@@ -1,9 +1,36 @@
 /* eslint-disable @next/next/no-img-element */
+import GlobalStateContext from "@/states/globalStateContext";
 import { useSession } from "next-auth/react";
-import React from "react";
+import { useRouter } from "next/router";
+import React, { useContext, useEffect, useState } from "react";
 
 function Sidenav() {
   const session = useSession();
+  const { sidenav_menu, tab, setTab } = useContext(GlobalStateContext) || [];
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!router.query.tab) {
+      setTab(0);
+    }
+    switch (router.query.tab) {
+      case "invoices":
+        setTab(0);
+        break;
+      case "paid":
+        setTab(1);
+        break;
+      case "due":
+        setTab(2);
+        break;
+      case "settings":
+        setTab(3);
+        break;
+      default:
+        setTab(0);
+        break;
+    }
+  }, [router.query]);
   return (
     <div className="bg-slate-100 w-72 lg:w-60 h-full overflow-y-auto pb-28 lg:pb-0 shrink-0">
       <div className="text-center p-8 font-poppins">
@@ -30,66 +57,25 @@ function Sidenav() {
         </button>
       </div>
       <ul className="p-4 mt-2 text-slate-700 text-sm font-medium space-y-2">
-        <li className="py-3 px-4 hover:bg-slate-200 bg-slate-200 rounded-md flex items-center space-x-3">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            class="w-4 h-4"
-          >
-            <path d="M18.375 2.25c-1.035 0-1.875.84-1.875 1.875v15.75c0 1.035.84 1.875 1.875 1.875h.75c1.035 0 1.875-.84 1.875-1.875V4.125c0-1.036-.84-1.875-1.875-1.875h-.75zM9.75 8.625c0-1.036.84-1.875 1.875-1.875h.75c1.036 0 1.875.84 1.875 1.875v11.25c0 1.035-.84 1.875-1.875 1.875h-.75a1.875 1.875 0 01-1.875-1.875V8.625zM3 13.125c0-1.036.84-1.875 1.875-1.875h.75c1.036 0 1.875.84 1.875 1.875v6.75c0 1.035-.84 1.875-1.875 1.875h-.75A1.875 1.875 0 013 19.875v-6.75z" />
-          </svg>
-
-          <span>Invoices</span>
-        </li>
-        <li className="py-3 px-4 hover:bg-slate-200 bg-slate-300/0 rounded-md flex items-center space-x-3">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            class="w-4 h-4"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
-              clip-rule="evenodd"
-            />
-          </svg>
-
-          <span>Paid</span>
-        </li>
-        <li className="py-3 px-4 hover:bg-slate-200 bg-slate-300/0/0 rounded-md flex items-center space-x-3">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            class="w-4 h-4"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zM12 8.25a.75.75 0 01.75.75v3.75a.75.75 0 01-1.5 0V9a.75.75 0 01.75-.75zm0 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z"
-              clip-rule="evenodd"
-            />
-          </svg>
-
-          <span>Due</span>
-        </li>
-        <li className="py-3 px-4 hover:bg-slate-200 bg-slate-300/0/0 rounded-md flex items-center space-x-3">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            class="w-4 h-4"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M11.078 2.25c-.917 0-1.699.663-1.85 1.567L9.05 4.889c-.02.12-.115.26-.297.348a7.493 7.493 0 00-.986.57c-.166.115-.334.126-.45.083L6.3 5.508a1.875 1.875 0 00-2.282.819l-.922 1.597a1.875 1.875 0 00.432 2.385l.84.692c.095.078.17.229.154.43a7.598 7.598 0 000 1.139c.015.2-.059.352-.153.43l-.841.692a1.875 1.875 0 00-.432 2.385l.922 1.597a1.875 1.875 0 002.282.818l1.019-.382c.115-.043.283-.031.45.082.312.214.641.405.985.57.182.088.277.228.297.35l.178 1.071c.151.904.933 1.567 1.85 1.567h1.844c.916 0 1.699-.663 1.85-1.567l.178-1.072c.02-.12.114-.26.297-.349.344-.165.673-.356.985-.57.167-.114.335-.125.45-.082l1.02.382a1.875 1.875 0 002.28-.819l.923-1.597a1.875 1.875 0 00-.432-2.385l-.84-.692c-.095-.078-.17-.229-.154-.43a7.614 7.614 0 000-1.139c-.016-.2.059-.352.153-.43l.84-.692c.708-.582.891-1.59.433-2.385l-.922-1.597a1.875 1.875 0 00-2.282-.818l-1.02.382c-.114.043-.282.031-.449-.083a7.49 7.49 0 00-.985-.57c-.183-.087-.277-.227-.297-.348l-.179-1.072a1.875 1.875 0 00-1.85-1.567h-1.843zM12 15.75a3.75 3.75 0 100-7.5 3.75 3.75 0 000 7.5z"
-              clip-rule="evenodd"
-            />
-          </svg>
-
-          <span>Settings</span>
-        </li>
+        {sidenav_menu.map((item, i) => {
+          return (
+            <li key={i}>
+              <button
+                onClick={() => {
+                  router.push(`/dashboard?tab=${item.tab}`, undefined, {
+                    shallow: true,
+                  });
+                }}
+                className={`py-3 px-4 ${
+                  tab == i && "bg-slate-200 active:bg-slate-200"
+                } lg:hover:bg-slate-200 active:bg-slate-200 w-full rounded-md flex items-center space-x-3 cursor-pointer`}
+              >
+                {item.icon}
+                <span>{item.name}</span>
+              </button>
+            </li>
+          );
+        })}
       </ul>
       <div className="p-6 font-poppins">
         <img
