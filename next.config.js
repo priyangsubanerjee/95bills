1c1957d5a4a068a75e66158c37fd82b9b8a25f59
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-05-17 18:12:04 +0530

    pwa

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+const withPWA = require("next-pwa")({
+  dest: "public",
+});
+
+module.exports = withPWA({
+  experimental: {
+    appDir: true, // <---- Comment and Uncomment this
+  },
+});
